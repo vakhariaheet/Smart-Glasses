f4535439a3c1a79352fa72030cc5fe042ba9c492
Heet Vakharia <heetkv@gmail.com>
2024-01-07 12:33:02 +0530

    Initial Commit

--- Readme.md
+++ Readme.md
@@ -43,6 +43,7 @@ The pin configuration is the following:
     cd ~
     wget https://github.com/rhasspy/piper/releases/download/v1.2.0/piper_armv7.tar.gz
     tar -xzvf piper_armv7.tar.gz
+    cd piper
     wget https://papeer.s3.ap-south-1.amazonaws.com/Audio.zip
     unzip -j Audio.zip
    ```
--- src/index.ts
+++ src/index.ts
@@ -5,40 +5,68 @@ import { textToSpeech, playSpeech } from './utils/TextToSpeech';
 
 const touchSensor = new Gpio(17, 'in', 'both');
 
+let timer: NodeJS.Timeout;
+let count = 0;
+
+
 touchSensor.watch(async (err, value) => {
 	if (err) {
 		throw err;
 	}
-	if (!value) {
-		const startTime = Date.now();
-		console.log('Capturing image...');
-		await capture();
-		const imageClickTime = Date.now();
-		console.log(`
-            Time taken to capture image: ${imageClickTime - startTime}ms
-            Current Elapsed Time: ${Date.now() - startTime}ms
-        `);
-		console.log('Image captured');
-		const text = await imageToText('test.jpeg');
-		const textClickTime = Date.now();
-		console.log(`
-            Time taken to generate text: ${textClickTime - imageClickTime}ms
-            Current Elapsed Time: ${Date.now() - startTime}ms
-        `);
-		console.log('Text generated');
-		console.log('Converting text to speech...');
-		await textToSpeech(text);
-		const speechClickTime = Date.now();
-		console.log(`
-            Time taken to generate speech: ${speechClickTime - textClickTime}ms
-            Current Elapsed Time: ${Date.now() - startTime}ms
-        `);
-		console.log('Speech generated');
-		console.log('Playing speech...');
-		await playSpeech();
-		console.log('Text spoken');
+	if (value) {
+		count++;
+		clearTimeout(timer);
+		timer = setTimeout(async () => { 
+			await tapHandler(count);
+			count = 0;
+		},300)
 	}
 });
+
+const tapHandler = async (count: number) => { 
+	if (count === 0) { 
+		// Long press
+	}
+	if (count === 1) {
+		await singleTapHandler();
+	}
+	else if (count === 2) {
+		// await doubleTapHandler();
+	}
+	else if (count === 3) { 
+		// await tripleTapHandler();
+	}
+}
+
+const singleTapHandler = async () => { 
+	const startTime = Date.now();
+	console.log('Capturing image...');
+	await capture();
+	const imageClickTime = Date.now();
+	console.log(`
+		Time taken to capture image: ${imageClickTime - startTime}ms
+		Current Elapsed Time: ${Date.now() - startTime}ms
+	`);
+	console.log('Image captured');
+	const text = await imageToText('test.jpeg');
+	const textClickTime = Date.now();
+	console.log(`
+		Time taken to generate text: ${textClickTime - imageClickTime}ms
+		Current Elapsed Time: ${Date.now() - startTime}ms
+	`);
+	console.log('Text generated');
+	console.log('Converting text to speech...');
+	await textToSpeech(text);
+	const speechClickTime = Date.now();
+	console.log(`
+		Time taken to generate speech: ${speechClickTime - textClickTime}ms
+		Current Elapsed Time: ${Date.now() - startTime}ms
+	`);
+	console.log('Speech generated');
+	console.log('Playing speech...');
+	await playSpeech();
+	console.log('Text spoken');
+}
 
 process.on('SIGINT', () => {
 	touchSensor.unexport();
