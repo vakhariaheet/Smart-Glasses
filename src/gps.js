693ab196576dc3e3444fea394b81f5e4c7622edd
Heet Vakharia <heetkv@gmail.com>
2024-01-17 15:13:57 +0530

    Added Docker

--- src/gps.js
+++ src/gps.js
@@ -13,11 +13,14 @@ listener.connect(() => {
 
   // Add a 'raw' listener to handle TPV data (Time, Position, Velocity)
   listener.on('TPV', (tpvData) => {
+    console.log(`Raw`,tpvData)
     if (tpvData.lat && tpvData.lon) {
       console.log(`Latitude: ${tpvData.lat}, Longitude: ${tpvData.lon}`);
     }
   });
-
+  listener.on('raw', (data) => {
+    console.log('Raw GPS Data:', data);
+  });
   // Start watching for TPV data
   listener.watch();
 });
