e7ca661dfa52ab644079cb8560a288e2a1df31fc
Heet Vakharia <heetkv@gmail.com>
2024-01-19 22:59:49 +0530

    Updated tsconfig

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,10 @@ import imageToText from './utils/Bard';
 import connectMongo from './utils/connectMongo';
 import { textToSpeech, playSpeech } from './utils/TextToSpeech';
 import { readTemperature } from './utils/Temperature';
-import startGPS from './utils/GPS';
+import  { getDistanceFromLatLonInKm } from './utils/GPS';
+import * as gpsd from 'node-gpsd';
+import { TPVDATA } from './types';
+import GPS from './Models/GPS';
 const touchSensor = new Gpio(17, 'in', 'both');
 const irSensor = new Gpio(27, 'in', 'falling');
 
@@ -13,7 +16,65 @@ let count = 0;
 
 
 connectMongo();
-startGPS();
+
+
+const listener = new gpsd.Listener({
+    port: 2947,
+    hostname: 'localhost',
+    device: '/dev/ttyS0', // Correct path for your GPS device
+    parse: true
+});
+listener.connect(() => {
+	console.log('Connected to gpsd');
+
+	// Add a 'raw' listener to handle TPV data (Time, Position, Velocity)
+	listener.on('TPV', async (tpvData: TPVDATA) => {
+		console.log(`Raw`, tpvData)
+		if (tpvData.lat && tpvData.lon) {
+			const lastEntry = await GPS.findOne().sort({ createdAt: -1 });
+			if (lastEntry) {
+				const lastEntryLat = lastEntry.latitude;
+				const lastEntryLon = lastEntry.longitude;
+				const distance = getDistanceFromLatLonInKm(
+					lastEntryLat,
+					lastEntryLon,
+					tpvData.lat,
+					tpvData.lon
+				);
+				if (distance > 0.001) {
+					const gps = new GPS({
+						latitude: tpvData.lat,
+						longitude: tpvData.lon,
+						speed: tpvData.speed,
+						altitude: tpvData.alt,
+						climb: tpvData.climb,
+						glassesId: '1'
+					});
+					await gps.save();
+				}
+
+			}
+			else {
+				const gps = new GPS({
+					latitude: tpvData.lat,
+					longitude: tpvData.lon,
+					speed: tpvData.speed,
+					altitude: tpvData.alt,
+					climb: tpvData.climb,
+					glassesId: '1'
+				});
+				await gps.save();
+			}
+		}
+	});
+	// Start watching for TPV data
+	listener.watch();
+});
+
+// Handle errors
+listener.on('error', (err: any) => {
+	console.error(`Error: ${err.message}`);
+});
 touchSensor.watch(async (err, value) => {
 	if (err) {
 		throw err;
--- src/utils/GPS.ts
+++ src/utils/GPS.ts
@@ -1,20 +1,10 @@
-import * as gpsd from 'node-gpsd';
-import { TPVDATA } from '../types';
-import GPS from '../Models/GPS';
 
-
-const listener = new gpsd.Listener({
-    port: 2947,
-    hostname: 'localhost',
-    device: '/dev/ttyS0', // Correct path for your GPS device
-    parse: true
-});
 
 const deg2rad = (deg: number) => {
     return deg * (Math.PI / 180)
 }
 
-const getDistanceFromLatLonInKm = (lat1: number, lon1: number, lat2: number, lon2: number) => {
+export const getDistanceFromLatLonInKm = (lat1: number, lon1: number, lat2: number, lon2: number) => {
     const R = 6371; // Radius of the earth in km
     const dLat = deg2rad(lat2 - lat1); // deg2rad below
     const dLon = deg2rad(lon2 - lon1);
@@ -28,58 +18,4 @@ const getDistanceFromLatLonInKm = (lat1: number, lon1: number, lat2: number, lon
     return d;
 }
 
-const startGPS = async () => {
-    listener.connect(() => {
-        console.log('Connected to gpsd');
 
-        // Add a 'raw' listener to handle TPV data (Time, Position, Velocity)
-        listener.on('TPV', async (tpvData: TPVDATA) => {
-            console.log(`Raw`, tpvData)
-            if (tpvData.lat && tpvData.lon) {
-                const lastEntry = await GPS.findOne().sort({ createdAt: -1 });
-                if (lastEntry) {
-                    const lastEntryLat = lastEntry.latitude;
-                    const lastEntryLon = lastEntry.longitude;
-                    const distance = getDistanceFromLatLonInKm(
-                        lastEntryLat,
-                        lastEntryLon,
-                        tpvData.lat,
-                        tpvData.lon
-                    );
-                    if (distance > 0.001) {
-                        const gps = new GPS({
-                            latitude: tpvData.lat,
-                            longitude: tpvData.lon,
-                            speed: tpvData.speed,
-                            altitude: tpvData.alt,
-                            climb: tpvData.climb,
-                            glassesId: '1'
-                        });
-                        await gps.save();
-                    }
-
-                }
-                else {
-                    const gps = new GPS({
-                        latitude: tpvData.lat,
-                        longitude: tpvData.lon,
-                        speed: tpvData.speed,
-                        altitude: tpvData.alt,
-                        climb: tpvData.climb,
-                        glassesId: '1'
-                    });
-                    await gps.save();
-                }
-            }
-        });
-        // Start watching for TPV data
-        listener.watch();
-    });
-
-    // Handle errors
-    listener.on('error', (err: any) => {
-        console.error(`Error: ${err.message}`);
-    });
-};
-
-export default startGPS;
