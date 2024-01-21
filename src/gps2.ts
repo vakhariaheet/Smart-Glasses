cc22b842908e9b4239848ae7b2cca8ddf364d9cc
Heet Vakharia <heetkv@gmail.com>
2024-01-21 16:55:23 +0530

    Updated tsconfig

--- src/gps2.ts
+++ src/gps2.ts
@@ -1,6 +1,7 @@
 import { SerialPort } from 'serialport';
 import { ReadlineParser } from '@serialport/parser-readline';
-import {parseNmeaSentence} from 'nmea-simple';
+import { parseNmeaSentence } from 'nmea-simple';
+import GPS from 'gps';
 const port = new SerialPort({
     path: '/dev/ttyS0',
     baudRate: 9600
@@ -8,11 +9,14 @@ const port = new SerialPort({
 
 
 const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
+const gps = new GPS;
+
+gps.on('data', data => {
+    console.log(data, gps.state);
+})
 
 parser.on('data', (data) => {
-    console.log(data);
-    const gpsData = parseNmeaSentence(data);
-    console.log(gpsData);
+    gps.updatePartial(data);
 });
 
 
