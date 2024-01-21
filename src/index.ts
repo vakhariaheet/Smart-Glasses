9acd85abc8fafa0a0db91899436463f59f8a71c3
Heet Vakharia <heetkv@gmail.com>
2024-01-21 18:35:10 +0530

    Updated Package

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,10 @@ import connectMongo from './utils/connectMongo';
 import { textToSpeech, playSpeech } from './utils/TextToSpeech';
 import { readTemperature } from './utils/Temperature';
 import  { getDistanceFromLatLonInKm } from './utils/GPS';
-import initGPS from './utils/GPS';
+import CurrentGPS from './Models/CurrentGPS';
+import GPSRoute from './Models/GPSRoute';
+import GPS, { GGA, RMC } from 'gps';
+import { ReadlineParser, SerialPort } from 'serialport';
 const touchSensor = new Gpio(17, 'in', 'both');
 const irSensor = new Gpio(27, 'in', 'falling');
 
@@ -14,7 +17,72 @@ let count = 0;
 
 
 connectMongo();
-initGPS();
+const port = new SerialPort({
+	path: '/dev/ttyS0',
+	baudRate: 9600
+});
+
+
+const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
+const gps = new GPS
+
+gps.on('data', async (data: GGA | RMC | any) => {
+	if (data.type !== 'GGA' || data.type !== 'RMC') return;
+	const lastEntry = await GPSRoute.findOne().sort({ createdAt: -1 });
+	if (data.type == 'GGA') {
+		if (lastEntry) {
+			const distance = getDistanceFromLatLonInKm(lastEntry.latitude, lastEntry.longitude, data.lat, data.lon);
+			if (distance > 0.1) {
+				const gpsData = new GPSRoute({
+					latitude: data.lat,
+					longitude: data.lon,
+					altitude: data.alt,
+					speed: null,
+					glassesId: '1',
+				});
+				gpsData.save();
+			}
+		}
+		else {
+			const gpsData = new GPSRoute({
+				latitude: data.lat,
+				longitude: data.lon,
+				altitude: data.alt,
+				speed: null,
+				glassesId: '1',
+			});
+			gpsData.save();
+		}
+	}
+	if (data.type == 'RMC') {
+		if (lastEntry) {
+			const distance = getDistanceFromLatLonInKm(lastEntry.latitude, lastEntry.longitude, data.lat, data.lon);
+			if (distance > 0.1) {
+				const currentGPS = new CurrentGPS({
+					latitude: data.lat,
+					longitude: data.lon,
+					speed: data.speed,
+					track: data.track,
+				});
+				currentGPS.save();
+			}
+		}
+		else {
+			const currentGPS = new CurrentGPS({
+				latitude: data.lat,
+				longitude: data.lon,
+				speed: data.speed,
+				track: data.track,
+			});
+			currentGPS.save();
+		}
+	}
+	console.log("GPS:", data);
+})
+
+parser.on('data', (data) => {
+	gps.update(data);
+});
 touchSensor.watch(async (err, value) => {
 	if (err) {
 		throw err;
