20155bf93a0f226c1e22f9cba45533f3103fd7f2
Heet Vakharia <heetkv@gmail.com>
2024-01-25 15:59:25 +0530

    Paused GPS

--- filerec.js
+++ filerec.js
@@ -1,24 +1,14 @@
-// Record audio and save to file
-
-var fs = require('fs');
-var recorder = require('node-record-lpcm16');
-
-
-const file = fs.createWriteStream('user.wav', { encoding: 'binary' });
-
-const recording = recorder.record({
-    sampleRate: 16000,
-    threshold: 0.5,
-    silence: '1.0',
-    audioType: 'wav',
+let Mic = require('node-microphone');
+let mic = new Mic();
+let micStream = mic.startRecording();
+micStream.pipe( myWritableStream );
+setTimeout(() => {
+    logger.info('stopped recording');
+    mic.stopRecording();
+}, 3000);
+mic.on('info', (info) => {
+	console.log(info);
 });
-
-recording
-.stream()
-.pipe(file)
-
-setTimeout(function () {
-	recording.stop();
-}, 5000);
-
-// Path: SmartGlassESraspi/record.js
+mic.on('error', (error) => {
+	console.log(error);
+});
