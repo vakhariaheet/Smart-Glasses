efb2b63537a01ecb3afc7150797dc509007ce1cd
Heet Vakharia <heetkv@gmail.com>
2024-01-25 16:04:01 +0530

    Paused GPS

--- filerec.js
+++ filerec.js
@@ -1,17 +1,30 @@
-let Mic = require('node-microphone');
+const Mic = require('node-microphone');
 const fs = require('fs');
-let mic = new Mic();
 
-let myWritableStream = fs.createWriteStream('test.wav');
-let micStream = mic.startRecording();
-micStream.pipe( myWritableStream );
+const mic = new Mic();
+const myWritableStream = fs.createWriteStream('test.wav');
+
+mic.on('info', (info) => {
+    console.log('Microphone info:', info);
+});
+
+mic.on('error', (error) => {
+    console.error('Microphone error:', error);
+});
+
+mic.on('data', (data) => {
+    // Here you can do something with the data if needed
+    console.log('Received data from microphone:', data.length, 'bytes');
+});
+
+mic.on('end', () => {
+    console.log('Microphone recording ended');
+});
+
+mic.startRecording()
+    .pipe(myWritableStream);
+
 setTimeout(() => {
-    console.log('stopping');
+    console.log('Stopping recording');
     mic.stopRecording();
 }, 3000);
-mic.on('info', (info) => {
-	console.log(info);
-});
-mic.on('error', (error) => {
-	console.log(error);
-});
