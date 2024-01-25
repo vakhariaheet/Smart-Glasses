acaea97d08eb265059cf8e1abb065db3a93bd1de
Heet Vakharia <heetkv@gmail.com>
2024-01-25 16:06:42 +0530

    Paused GPS

--- filerec.js
+++ filerec.js
@@ -1,30 +1,57 @@
-const Mic = require('node-microphone');
-const fs = require('fs');
-
-const mic = new Mic();
-const myWritableStream = fs.createWriteStream('test.wav');
-
-mic.on('info', (info) => {
-    console.log('Microphone info:', info);
+var mic = require('mic');
+var fs = require('fs');
+ 
+var micInstance = mic({
+    rate: '16000',
+    channels: '1',
+    debug: true,
+    exitOnSilence: 6
 });
-
-mic.on('error', (error) => {
-    console.error('Microphone error:', error);
+var micInputStream = micInstance.getAudioStream();
+ 
+var outputFileStream = fs.WriteStream('output.raw');
+ 
+micInputStream.pipe(outputFileStream);
+ 
+micInputStream.on('data', function(data) {
+    console.log("Recieved Input Stream: " + data.length);
 });
-
-mic.on('data', (data) => {
-    // Here you can do something with the data if needed
-    console.log('Received data from microphone:', data.length, 'bytes');
+ 
+micInputStream.on('error', function(err) {
+    cosole.log("Error in Input Stream: " + err);
 });
-
-mic.on('end', () => {
-    console.log('Microphone recording ended');
+ 
+micInputStream.on('startComplete', function() {
+    console.log("Got SIGNAL startComplete");
+    setTimeout(function() {
+            micInstance.pause();
+    }, 5000);
 });
-
-mic.startRecording()
-    .pipe(myWritableStream);
-
-setTimeout(() => {
-    console.log('Stopping recording');
-    mic.stopRecording();
-}, 3000);
+    
+micInputStream.on('stopComplete', function() {
+    console.log("Got SIGNAL stopComplete");
+});
+    
+micInputStream.on('pauseComplete', function() {
+    console.log("Got SIGNAL pauseComplete");
+    setTimeout(function() {
+        micInstance.resume();
+    }, 5000);
+});
+ 
+micInputStream.on('resumeComplete', function() {
+    console.log("Got SIGNAL resumeComplete");
+    setTimeout(function() {
+        micInstance.stop();
+    }, 5000);
+});
+ 
+micInputStream.on('silence', function() {
+    console.log("Got SIGNAL silence");
+});
+ 
+micInputStream.on('processExitComplete', function() {
+    console.log("Got SIGNAL processExitComplete");
+});
+ 
+micInstance.start();
