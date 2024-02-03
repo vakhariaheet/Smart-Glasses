7588c47d281b4b0330145c33142cadb016b5d5db
Heet Vakharia <heetkv@gmail.com>
2024-02-04 02:08:33 +0530

    Added SFX

--- misc.ts
+++ misc.ts
@@ -1,37 +1,22 @@
-import { ChildProcess, exec } from 'child_process';
 import PlaySound from 'play-sound';
 import { setTimeout } from 'timers/promises';
 
-const player = PlaySound({});
+const player = PlaySound({
+    player: 'mplayer'
+});
 
 (async () => {
-    const playSoundLoop = (path: string, loop: boolean = false) => {
-        let childProcess: ChildProcess | null = null;
+    const playSpeechSync = (path?: string, loop?: boolean) => {
+        return player.play(path || 'welcome.mp3', {
+            mplayer: [ '-loop', loop ? '0' : '999' ]
+        }, function (err) {
+            if (err) throw err
 
-        const playSound = () => {
-            childProcess = exec(`aplay ${path}`, (err) => {
-                if (err) {
-                    console.error('Error playing sound:', err);
-                } else if (loop) {
-                    playSound(); // Play the sound again if loop is true
-                }
-            });
-        };
-
-        playSound();
-
-        const kill = () => {
-            if (childProcess) {
-                childProcess.kill();
-            }
-        };
-
-        return { kill, childProcess };
-    };
-    const res = playSoundLoop('./src/assets/sfx/loading.mp3', true);
+        })
+    }
+    const res = playSpeechSync('./src/assets/sfx/start.mp3', true);
     await setTimeout(4000);
     res.kill();
-    console.log('Done');
 })();
 
 
