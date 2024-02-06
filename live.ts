4fd3d057e9a4e74b69309aa260b856bff397aa22
Heet Vakharia <heetkv@gmail.com>
2024-02-06 15:52:39 +0530

    Fixed Timer issue

--- live.ts
+++ live.ts
@@ -2,7 +2,7 @@ import raspividStream from 'raspivid-stream';
 import { io } from 'socket.io-client';
 
 const stream = raspividStream();
-const socket = io('http://localhost:3000');
+const socket = io('http://192.168.1.5:3000');
 
 
 console.log('Connected to server');
@@ -11,3 +11,7 @@ stream.on('data', (data: any) => {
     socket.emit('video', data);
 });
 
+// Handle disconnection
+socket.on('disconnect', () => {
+    console.log('Disconnected from server');
+});
