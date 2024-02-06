fd1d152b55c15654391103e58eda0f80bae138f4
Heet Vakharia <heetkv@gmail.com>
2024-02-06 15:17:28 +0530

    Fixed Timer issue

--- live.ts
+++ live.ts
@@ -1,20 +1,43 @@
-import raspividStream from 'raspivid-stream';
-import { io } from 'socket.io-client';
+const io = require('socket.io-client');
+const { StreamCamera, Codec, Flip, SensorMode } = require('pi-camera-connect');
+const socket = io.connect('http://xxx.xxx.xxx.xxx:3000/iot');
 
-const stream = raspividStream();
-const socket = io('http://localhost:3000');
-
-
-console.log('Connected to server');
-
-stream.on('data', (data: any) => {
-    socket.emit('video', data);
+const streamCamera = new StreamCamera({
+    codec: Codec.MJPEG,
+    flip: Flip.Vertical,
+    sensorMode: SensorMode.Mode6
 });
 
-socket.on('disconnect', () => {
-    stream.removeAllListeners('data');
-    stream.removeAllListeners('end');
-    stream.removeAllListeners('error');
-    stream.end();
-    console.log('Disconnected');
+
+socket.on('connect', () => {
+    socket.sendBuffer = [];
+
+    socket.emit("pi-cam-init", "Cam-1");
+
+    console.log("Connected to the server!" + socket.id);
+})
+
+socket.on('new-consumer', (data) => {
+    console.log(data + ' has join the stream');
+});
+
+socket.on('consumer-left', (data) => {
+    console.log(data + ' has left the stream');
+});
+
+streamCamera.on('frame', (data) => {
+    socket.binary(true).emit('video', 'Cam-1', "data:image/jpeg;base64," + data.toString("base64"));
+});
+
+
+async function cameraStartCapture() {
+    await streamCamera.startCapture();
+}
+
+async function cameraStopCapture() {
+    await streamCamera.stopCapture();
+}
+
+cameraStartCapture().then(() => {
+    console.log('Camera is now capturing');
 });
