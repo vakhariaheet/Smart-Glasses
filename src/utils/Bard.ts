a6130ee39e9248939888e429bb00f949dcff3d15
Heet Vakharia <heetkv@gmail.com>
2024-10-16 00:35:33 +0530

    Updated Gemini API and Optimise Prompts

--- src/utils/Bard.ts
+++ src/utils/Bard.ts
@@ -1,31 +1,34 @@
 import { GoogleGenerativeAI } from '@google/generative-ai';
+import { GoogleAIFileManager } from "@google/generative-ai/server";
 import fs from 'fs';
 import dotenv from 'dotenv';
 dotenv.config();
 const genAI = new GoogleGenerativeAI(process.env.API_KEY as string);
+const fileManager = new GoogleAIFileManager(process.env.API_KEY as string);
+const fileToGenerativePart = async (path: string, mimeType: string) => {
+	const uploadResponse = await fileManager.uploadFile(path, {
+		mimeType,
+		displayName: 'test.jpeg',
+		
+	});
 
-const fileToGenerativePart = (path: string, mimeType: string) => {
-	const data = Buffer.from(fs.readFileSync(path)).toString('base64');
-	console.log(`Size of ${path}: ${data.length} bytes (i.e. ${data.length / 1024 / 1024} MB)`);
-	return {
-		inlineData: {
-			data,
-			mimeType,
-		},
-	};
+	return uploadResponse;
+
 }
 
 async function imageToText(image: string) {
 	// For text-and-image input (multimodal), use the gemini-pro-vision model
-	const model = genAI.getGenerativeModel({ model: 'gemini-pro-vision' });
+	const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
 	console.log(`Generating...`)
 	const prompt = `Describe this scene as if narrating to someone who can't see it. Be detailed but natural, avoiding any mention of an image. Use only elements present in the scene. Keep your description concise, under 100 words, while capturing the essence of what's visible.`;
 
-	const imageParts = [
-		fileToGenerativePart(image, 'image/jpeg'),
-	];
-
-	const result = await model.generateContent([ prompt, ...imageParts ]);
+	const uploadedImage = await fileToGenerativePart(image, 'image/jpeg');
+	const result = await model.generateContent([ prompt, {
+		fileData: {
+			fileUri: uploadedImage.file.uri,
+			mimeType: uploadedImage.file.mimeType,
+		}
+	} ]);
 	const response = await result.response;
 	const text = response.text();
 	console.log(`Generated: ${text}`)
@@ -35,16 +38,16 @@ async function imageToText(image: string) {
 export const detectCurrency = async (image: string) => {
 	const model = genAI.getGenerativeModel({ model: 'gemini-pro-vision' });
 	console.log(`Generating...`)
-	const prompt = `
-	Identify the currency in the image
-	1. Respond with the currency name and denomination.
-	`;
+	const prompt = `Analyze the image and identify the currency. Provide the name of the currency and its denomination.`;
 
-	const imageParts = [
-		fileToGenerativePart(image, 'image/jpeg'),
-	];
+	const uploadedImage = await fileToGenerativePart(image, 'image/jpeg');
 
-	const result = await model.generateContent([ prompt, ...imageParts ]);
+	const result = await model.generateContent([ prompt,  {
+		fileData: {
+			fileUri: uploadedImage.file.uri,
+			mimeType: uploadedImage.file.mimeType,
+		}
+	} ]);
 	const response = await result.response;
 	const text = response.text();
 	console.log(`Generated: ${text}`)
@@ -55,13 +58,8 @@ export const detectCurrency = async (image: string) => {
 export const generateText = async (prompt: string) => {
 	const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
 	console.log(`Generating...`)
-	const result = await model.generateContent([ `
-	1. Generate a text based on the prompt
-	2. If there is Something like ask visio or hey visio remove it
-	3. prompt: ${prompt}
-	4. Generate text should be short and precise under 100 words unless previously specified.
-	5. Don't say like here is the generated text
-	` ]);
+	const result = await model.generateContent([ `Generate text based on the provided prompt. Remove any phrases like 'ask visio' or 'hey visio.' Ensure the text is concise (under 100 words unless otherwise specified). Avoid introductory phrases such as 'here is the generated text.'
+	prompt: ${prompt}` ]);
 	const response = await result.response;
 	const text = response.text();
 	console.log(`Generated: ${text}`)
